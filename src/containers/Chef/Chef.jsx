6660e8c59fe2da3990b1d6e3a08e233a3d470aaf
Robert McCarter <69922179+robertmc5@users.noreply.github.com>
2023-06-14 12:38:36 -0400

    feat: develop Chef

--- src/containers/Chef/Chef.jsx
+++ src/containers/Chef/Chef.jsx
@@ -1,9 +1,37 @@
+
 import React from 'react';
+
+import './Chef.css';
+import { images } from '../../constants';
+import { SubHeading } from '../../components';
 
 const Chef = () => {
   return (
-    <div>
-      Chef
+    <div className='app__bg app__wrapper section__padding'>
+
+      <div className='app__wrapper_img app__wrapper_img-reverse'>
+        <img src={images.chef} alt="Chef" />
+      </div>
+
+      <div className='app__wrapper_info'>
+        <SubHeading title="Chef's Word"/>
+        <h2 className='headtext__cormorant'>What We Believe In</h2>
+        <div className='app__chef-content'>
+          <div className='app__chef-content_quote'>
+            <img src={images.quote} alt="Quote" />
+            <p className='p__opensans'>Lorem ipsum dolor sit amet, consectetur adipiscing elit auctor sit.</p>
+          </div>
+          <p className='p__opensans'>auctor sit iaculis in arcu. Vulputate nulla lobortis mauris eget sit. 
+              Nulla scelerisque scelerisque congue ac consequat, aliquam molestie lectus eu. 
+              Congue iaculis integer curabitur semper sit nunc.</p>
+        </div>
+        <div className='app__chef-sign'>
+          <p>Kevin Luo</p>
+          <p className='p__opensans' style={{color: '#AAA'}}>Chef & Founder</p>
+          <img src={images.sign} alt="Chef signature" />
+        </div>
+      </div>
+
     </div>
   )
 }
